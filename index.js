538f14bd6a3b13234adfa49b5715a53c32f4f6ba
Heesu Jung <topjung3@gmail.com>
2020-05-11 06:16:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const {execSync} = require('child_process')
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 var fs = require('fs');
 
 console.log(process.cwd())
@@ -6,6 +9,9 @@ console.log(process.cwd())
 fs.readdir("./", function(error, filelist){
   console.log(filelist);
 })
+
+
+console.log(github.GITHUB_WORKFLOW)
 
 
 console.log(execSync("cp " + __dirname + "/hello.txt ./" ).toString())
